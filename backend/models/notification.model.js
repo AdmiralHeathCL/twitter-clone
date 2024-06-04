a26b9511a79d748dab5bbb128703a3daad3c7866
AdmiralHeathCL <cliffishealthy@gmail.com>
2024-06-04 00:11:17 -0400

    1- post routes and controllers added

--- backend/models/notification.model.js
+++ backend/models/notification.model.js
@@ -4,24 +4,24 @@ const notificationSchema = new mongoose.Schema({
     from: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
+        required: true,
     },
     to: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
+        required: true,
     },
     type: {
         type: String,
         required: true,
-        enum: ['follow', 'like']
+        enum: ['follow', 'like'],
     },
     read: {
         type: Boolean,
-        default: false
-    }
+        default: false,
+    },
 }, {timestamps: true});
 
-const Notification = mongoose.model('Notification', notificationSchema)
+const Notification = mongoose.model("Notification", notificationSchema);
 
 export default Notification;
