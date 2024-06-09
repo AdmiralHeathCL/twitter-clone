d97f6c264ac8eb5ed81edd70f688d3e453ef2801
AdmiralHeathCL <cliffishealthy@gmail.com>
2024-06-09 01:05:52 -0400

    3- payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 
 //console.log(process.env.MONGO_URI);
  
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
